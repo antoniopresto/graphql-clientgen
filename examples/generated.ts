c246278b3be6350cee4848d32749b27de0032b8c
antoniopresto <antoniopresto@gmail.com>
2019-06-20 12:23:07 -0300

    beta0

--- examples/generated.ts
+++ examples/generated.ts
@@ -1,75 +1,3 @@
-export type FetcherConfig<V = any> = {
-  headers?: { [key: string]: string };
-  query: string;
-  entityName: string;
-  schemaKey: string;
-  transformFetchConfig?: (config: {
-    requestInit: RequestInit;
-    variables: V;
-    config: FetcherConfig<V>;
-  }) => RequestInit;
-  fragment?: string;
-  [key: string]: any;
-};
-
-let API_URL = 'http://localhost:3777/graphql';
-
-let queryFetcher = function queryFetcher<Variables, Return>(
-  variables: Variables,
-  config: FetcherConfig<Variables>
-): Promise<Return> {
-  let requestInit: RequestInit = {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      ...config.headers
-    },
-    body: JSON.stringify({
-      query: config.query,
-      variables
-    })
-  };
-
-  if (config.transformFetchConfig) {
-    requestInit = config.transformFetchConfig({
-      requestInit,
-      variables,
-      config
-    });
-  }
-
-  return fetch(API_URL, requestInit).then(async response => {
-    const contentType = response.headers.get('Content-Type');
-    if (contentType && contentType.startsWith('application/json')) {
-      const { errors, data } = await response.json();
-
-      if (errors) {
-        throw new Error(errors.join('\n'));
-      }
-
-      return data[config.schemaKey];
-    } else {
-      throw new Error(await response.text());
-    }
-  });
-};
-
-export type QueryFetcher = typeof queryFetcher;
-
-export const setQueryFetcher = (fetcher: QueryFetcher) => {
-  queryFetcher = fetcher;
-};
-
-export const getQueryfetcher = () => queryFetcher;
-
-export const setApiUrl = (url: string) => {
-  API_URL = url;
-};
-
-export const getApiUrl = () => API_URL;
-
 export const query = {
   cities: (fragment = '') => `
         query cities($filter: CityFilterInput, $limit: Int, $skip: Int){
@@ -149,28 +77,134 @@ export type QueryCreateCityArgs = {
   payload?: Maybe<CreateCityPayload>;
 };
 
-export const client = {
-  cities: (variables: QueryCitiesArgs, config: Partial<FetcherConfig> = {}) => {
-    return queryFetcher<QueryCitiesArgs, Maybe<Query['cities']>>(variables, {
-      entityName: 'City',
-      schemaKey: 'cities',
-      query: query.cities(config.fragment),
-      ...config
-    });
-  },
+type MiddlewareContext<V> = {
+  requestConfig: RequestInit;
+  variables: V;
+  config: FetcherConfig<V>;
+};
 
-  createCity: (
-    variables: QueryCreateCityArgs,
-    config: Partial<FetcherConfig> = {}
-  ) => {
-    return queryFetcher<QueryCreateCityArgs, Maybe<Query['createCity']>>(
-      variables,
-      {
-        entityName: 'City',
-        schemaKey: 'createCity',
-        query: query.createCity(config.fragment),
-        ...config
-      }
-    );
+type FetchMiddleware<V> = (
+  config: MiddlewareContext<V>
+) => MiddlewareContext<V>;
+
+export type FetcherConfig<V = any> = {
+  apiURL: string;
+  headers?: { [key: string]: string };
+  query: string;
+  entityName: string;
+  schemaKey: string;
+  middlewareList?: FetchMiddleware<V>[];
+  fragment?: string;
+};
+
+const queryFetcher = async function queryFetcher<Variables, Return>(
+  variables: Variables,
+  config: FetcherConfig<Variables>
+): Promise<Return> {
+  let requestConfig: RequestInit = {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      ...config.headers
+    },
+    body: JSON.stringify({
+      query: config.query,
+      variables
+    })
+  };
+
+  let context: MiddlewareContext<Variables> = {
+    requestConfig,
+    variables,
+    config
+  };
+
+  if (config.middlewareList) {
+    context = applyMiddleware(config.middlewareList)(context);
+    console.log(context);
   }
+
+  return fetch(context.config.apiURL, context.requestConfig).then(
+    async response => {
+      const contentType = response.headers.get('Content-Type');
+      if (contentType && contentType.startsWith('application/json')) {
+        const { errors, data } = await response.json();
+
+        if (errors) {
+          throw new Error(errors.join('\n'));
+        }
+
+        return data[config.schemaKey];
+      } else {
+        throw new Error(await response.text());
+      }
+    }
+  );
+};
+
+export type QueryFetcher = typeof queryFetcher;
+
+export class GraphQLClient {
+  apiURL = '/graphql';
+  middlewareList: FetchMiddleware<any>[] = [];
+
+  exec = <V, R>(_variables: V, _config: FetcherConfig<V>) => {
+    return queryFetcher<V, R>(_variables, {
+      apiURL: this.apiURL,
+      ..._config,
+      middlewareList: [...this.middlewareList, ..._config.middlewareList]
+    });
+  };
+
+  actions = {
+    cities: (
+      variables: QueryCitiesArgs,
+      config: Partial<FetcherConfig> = {}
+    ) => {
+      return this.exec<QueryCitiesArgs, Maybe<Query['cities']>>(variables, {
+        apiURL: this.apiURL,
+        entityName: 'City',
+        schemaKey: 'cities',
+        query: query.cities(config.fragment),
+        ...config
+      });
+    },
+
+    createCity: (
+      variables: QueryCreateCityArgs,
+      config: Partial<FetcherConfig> = {}
+    ) => {
+      return this.exec<QueryCreateCityArgs, Maybe<Query['createCity']>>(
+        variables,
+        {
+          apiURL: this.apiURL,
+          entityName: 'City',
+          schemaKey: 'createCity',
+          query: query.createCity(config.fragment),
+          ...config
+        }
+      );
+    }
+  };
+}
+
+// compose(f, g, h) is identical to doing (...args) => f(g(h(...args))).
+function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (...args) => a(b(...args)));
+}
+
+const applyMiddleware = <V = any>(middlewareList: FetchMiddleware<V>[]) => {
+  return (context: MiddlewareContext<V>) => {
+    return compose(...middlewareList)(context);
+  };
 };
--- src/lib/clientbase.ts
+++ src/lib/clientbase.ts
@@ -1,26 +1,28 @@
+type MiddlewareContext<V> = {
+  requestConfig: RequestInit;
+  variables: V;
+  config: FetcherConfig<V>;
+};
+
+type FetchMiddleware<V> = (
+  config: MiddlewareContext<V>
+) => MiddlewareContext<V>;
+
 export type FetcherConfig<V = any> = {
+  apiURL: string;
   headers?: { [key: string]: string };
   query: string;
   entityName: string;
   schemaKey: string;
-  transformFetchConfig?: (
-    config: {
-      requestInit: RequestInit;
-      variables: V;
-      config: FetcherConfig<V>;
-    }
-  ) => RequestInit;
+  middlewareList?: FetchMiddleware<V>[];
   fragment?: string;
-  [key: string]: any;
 };
 
-let API_URL = 'http://localhost:3777/graphql';
-
-let queryFetcher = function queryFetcher<Variables, Return>(
+const queryFetcher = async function queryFetcher<Variables, Return>(
   variables: Variables,
   config: FetcherConfig<Variables>
 ): Promise<Return> {
-  let requestInit: RequestInit = {
+  let requestConfig: RequestInit = {
     method: 'POST',
     credentials: 'include',
     headers: {
@@ -34,40 +36,67 @@ let queryFetcher = function queryFetcher<Variables, Return>(
     })
   };
 
-  if (config.transformFetchConfig) {
-    requestInit = config.transformFetchConfig({
-      requestInit,
-      variables,
-      config
-    });
+  let context: MiddlewareContext<Variables> = {
+    requestConfig,
+    variables,
+    config
+  };
+
+  if (config.middlewareList) {
+    context = applyMiddleware(config.middlewareList)(context);
+    console.log(context);
   }
 
-  return fetch(API_URL, requestInit).then(async response => {
-    const contentType = response.headers.get('Content-Type');
-    if (contentType && contentType.startsWith('application/json')) {
-      const { errors, data } = await response.json();
+  return fetch(context.config.apiURL, context.requestConfig).then(
+    async response => {
+      const contentType = response.headers.get('Content-Type');
+      if (contentType && contentType.startsWith('application/json')) {
+        const { errors, data } = await response.json();
 
-      if (errors) {
-        throw new Error(errors.join('\n'));
+        if (errors) {
+          throw new Error(errors.join('\n'));
+        }
+
+        return data[config.schemaKey];
+      } else {
+        throw new Error(await response.text());
       }
-
-      return data[config.schemaKey];
-    } else {
-      throw new Error(await response.text());
     }
-  });
+  );
 };
 
 export type QueryFetcher = typeof queryFetcher;
 
-export const setQueryFetcher = (fetcher: QueryFetcher) => {
-  queryFetcher = fetcher;
+export class GraphQLClient {
+  apiURL = '/graphql';
+  middlewareList: FetchMiddleware<any>[] = [];
+
+  exec = <V, R>(_variables: V, _config: FetcherConfig<V>) => {
+    return queryFetcher<V, R>(_variables, {
+      apiURL: this.apiURL,
+      ..._config,
+      middlewareList: [...this.middlewareList, ..._config.middlewareList]
+    });
+  };
+
+  //[actions]//
+}
+
+// compose(f, g, h) is identical to doing (...args) => f(g(h(...args))).
+function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (...args) => a(b(...args)));
+}
+
+const applyMiddleware = <V = any>(middlewareList: FetchMiddleware<V>[]) => {
+  return (context: MiddlewareContext<V>) => {
+    return compose(...middlewareList)(context);
+  };
 };
-
-export const getQueryfetcher = () => queryFetcher;
-
-export const setApiUrl = (url: string) => {
-  API_URL = url;
-};
-
-export const getApiUrl = () => API_URL;
