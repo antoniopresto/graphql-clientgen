9856c63a8e418647f0fc3aa5a5267e875c32bdae
antoniopresto <antoniopresto@gmail.com>
2019-07-02 09:58:58 -0300

    tests: fix react import

--- src/__tests__/store.spec.ts
+++ src/__tests__/store.spec.ts
@@ -12,33 +12,33 @@ const filePaths = {
   provider: dest + '/Provider.js'
 };
 
-test('write generated JS files', async t => {
+test.serial('write generated JS files', async t => {
   const response = await printFromEndpoint(TEST_API);
   if (response.status !== 'ok') {
     throw new Error('invalid printFromEndpoint response');
   }
-  const { client, store } = response;
+  const { client, store, provider } = response;
 
   const clientJS = transpileTSSource(client);
   const storeJS = transpileTSSource(store);
-  // const providerJS = transpileTSSource(provider);
+  const providerJS = transpileTSSource(provider);
 
   await fs.mkdirp(dest);
   await fs.writeFile(filePaths.client, clientJS);
   await fs.writeFile(filePaths.store, storeJS);
-  // await fs.writeFile(filePaths.provider, providerJS);
+  await fs.writeFile(filePaths.provider, providerJS);
 
   t.is(clientJS.length > 100, true);
   t.is(storeJS.length > 100, true);
-  // t.is(providerJS.length > 100, true);
+  t.is(providerJS.length > 100, true);
 });
 
-test('should import generated files', async t => {
+test.serial('should import generated files', async t => {
   const client = await import(filePaths.client);
-  // const provider = await import(filePaths.provider);
+  const provider = await import(filePaths.provider);
   const store = await import(filePaths.store);
 
   t.is(client.__esModule, true);
-  // t.is(provider.__esModule, true);
+  t.is(provider.__esModule, true);
   t.is(store.__esModule, true);
 });
--- src/template/Provider.tsx
+++ src/template/Provider.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 import { GraphQLStore, StoreState } from './Store';
 import {
